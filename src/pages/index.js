901508897a2e9145d95f3da39cacb1d1b2bffd08
Duque <gentlepoop@gmail.com>
2023-04-30 17:40:00 -0400

    se añadió chakra

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+} from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
